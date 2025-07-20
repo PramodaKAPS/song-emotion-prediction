8080518788bbf0c08d806201a1536b13cd8d120b
Priyadarshana K.A.P.S. <121634558+PramodaKAPS@users.noreply.github.com>
2025-07-20 18:29:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from IPython import get_ipython
+ipython = get_ipython()
+if ipython is None:
+    print("Kernel not found. Run in a Colab notebook cell.")
+    exit(1)
 # main.py: Main script to run the full pipeline
 
 from setup import setup_environment
