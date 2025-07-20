d2750215c0fdebdd1024c18cf3c42f864569e2bf
Priyadarshana K.A.P.S. <121634558+PramodaKAPS@users.noreply.github.com>
2025-07-20 10:34:37 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,22 +1,11 @@
 def calculate_linear_arousal(audio_df):
-    weights = {
-        'tempo': 0.4,
-        'loudness': 0.3,
-        'energy': 0.2,
-        'speechiness': 0.05,
-        'danceability': 0.05
-    }
+    weights = {'tempo': 0.4, 'loudness': 0.3, 'energy': 0.2, 'speechiness': 0.05, 'danceability': 0.05}
     arousal = sum(weights[f] * audio_df[f] for f in weights)
     arousal = (arousal - arousal.min()) / (arousal.max() - arousal.min() + 1e-10)
     return arousal
 
 def calculate_linear_valence(audio_df):
-    weights = {
-        'energy': 0.5,
-        'mode': 0.25,
-        'tempo': 0.15,
-        'danceability': 0.1
-    }
+    weights = {'energy': 0.5, 'mode': 0.25, 'tempo': 0.15, 'danceability': 0.1}
     valence = sum(weights[f] * audio_df[f] for f in weights)
     valence = (valence - valence.min()) / (valence.max() - valence.min() + 1e-10)
     return valence
