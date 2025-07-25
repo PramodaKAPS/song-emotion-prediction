9784b91a4aab06e79f9d93316fbd39f1949e395f
Priyadarshana K.A.P.S. <121634558+PramodaKAPS@users.noreply.github.com>
2025-07-25 18:54:56 +0530

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -6,20 +6,19 @@ from scipy.stats import pearsonr
 def make_predictions(arousal_model, valence_model, lyrics_embeddings, song_df):
     if not song_df.empty and lyrics_embeddings.size > 0:
         X_text_lyrics = np.hstack([lyrics_embeddings, song_df[['xanew_arousal', 'xanew_valence']].values])
-        arousal_text = pd.Series(arousal_model.predict(X_text_lyrics, batch_size=32).flatten())
-        valence_text = pd.Series(valence_model.predict(X_text_lyrics, batch_size=32).flatten())
+        arousal_text = pd.Series(arousal_model.predict(X_text_lyrics, batch_size=16).flatten())
+        valence_text = pd.Series(valence_model.predict(X_text_lyrics, batch_size=16).flatten())
     else:
         arousal_text = pd.Series()
         valence_text = pd.Series()
     return arousal_text, valence_text
 
 def combine_predictions(arousal_text, valence_text, arousal_audio, valence_audio):
-    w_text = 0.46
-    w_audio = 0.54
+    w_text = 0.6
+    w_audio = 0.4
     arousal_final = w_text * arousal_text + w_audio * arousal_audio if not arousal_text.empty else arousal_audio
     valence_final = w_text * valence_text + w_audio * valence_audio if not valence_text.empty else valence_audio
 
-    # Scale final arousal and valence to [-1, 1]
     arousal_final = 2 * arousal_final - 1
     valence_final = 2 * valence_final - 1
     return arousal_final, valence_final
